9ec42b17a8272a2790f9ab282d3361f7f9717c10
Mateusz Tylec <mateusztylec@duck.com>
2025-04-03 15:40:44 +0200

    status changed

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ const projects: Project[] = [
         title: 'rhea.chat',
         description: 'chatbot with flowless access to many tools',
         url: 'https://rhea.chat/?ref=mateusztylec.com',
-        status: 'building',
+        status: 'live',
         stack: ['langgraph', 'ts', 'supabase', 'railway', 'llm', 'nextjs']
     },
     {
