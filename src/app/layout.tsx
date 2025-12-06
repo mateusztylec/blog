b7da9b88e9dc8755ecf2a6fa2ee43c5b50a1fb33
Mateusz Tylec <mateusztylec@duck.com>
2025-12-06 12:56:03 +0100

    fix: package bump

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,42 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
 
 
 const geistSans = localFont({
-    src: "./fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
 });
 const geistMono = localFont({
-    src: "./fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
 export const metadata: Metadata = {
-    title: 'mateusztylec',
-    description: 'Personal portfolio website',
+  title: 'mateusztylec',
+  description: 'Personal portfolio website',
 }
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <head>
-                <script
-                    defer
-                    data-domain="mateusztylec.com"
-                    src="https://plausible-prod.matsurge.com/js/script.js"
-                ></script>
-            </head>
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <head>
+        <script
+          defer
+          data-domain="mateusztylec.com"
+          src="https://plausible-prod.matsurge.com/js/script.js"
+        ></script>
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
 }
